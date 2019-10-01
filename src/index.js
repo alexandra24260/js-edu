5fb3353cd866e90d3c0ea57bf9af95a7cb9bb416
Alexandra Vecher <alex192004@inbox.ru>
2019-10-01 12:20:02 +0300

    resolved task JS EDU

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
+module.exports = function getTimeForEducation(   focus = 'family',    knowsProgramming = true,   config = {family: 4}   ) {
+    if (knowsProgramming === true) {
+        knowsProgramming = 800;
+    } else {
+        knowsProgramming = 1300;
+    }
+    var weeks = Math.ceil(knowsProgramming/config[focus]);
+    return weeks;
   };
   
